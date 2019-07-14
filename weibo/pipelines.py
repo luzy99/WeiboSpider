9ba60f256d2d49c4cf93819c1693fdb2c37d66d7
luzy99 <1694957926@qq.com>
2019-07-14 16:53:03 +0800

    first

---
+++ weibo/pipelines.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+from weibo import settings
+
+
+class WeiboPipeline(object):
+    # def __init__(self):
+    #     host = '127.0.0.1'
+    #     port = 27017
+    #     dbName = 'mydb'
+    #     client = pymongo.MongoClient(host=host, port=port)
+    #     tdb = client[dbName]
+    #     self.post = tdb['new']
+
+    # def process_item(self, item, spider):
+    #     article = dict(item)
+    #     self.post.insert(article)
+    #     return item
+    def __init__(self):
+        self.conn = pymysql.connect(host=settings.MYSQL_HOST, user=settings.MYSQL_USER,
+                                    passwd=settings.MYSQL_PASSWD, db=settings.MYSQL_DBNAME, charset='utf8')
+        self.cur = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        mid = item.get("mid", "N/A")    # 有的图书有数据项缺失，这里做了容错处理
+        userid = item.get("userid", "N/A")
+        verified_type = item.get("verified_type", "N/A")
+        text = item.get("text", "N/A")
+        created_at = item.get("created_at", "N/A")
+
+        sql = "insert ignore into rootknot(mid, userid, verified_type, text, created_at) VALUES (%s, %s, %s, %s, %s)"
+        self.cur.execute(sql, (mid, userid, verified_type, text, created_at))
+        self.conn.commit()
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
---
+++ weibo/spiders/rootknot.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+import re
+from ..items import WeiboItem
+import time
+
+
+class WeiboSpider(scrapy.Spider):
+    name = 'rootknot'
+    allowed_domains = ['m.weibo.cn']
+    key = '东南大学'
+    start_urls = [
+        ('https://m.weibo.cn/api/container/getIndex?containerid=100103type%3D60%26q%3D'+key+'&page_type=searchall')]
+
+    def parse(self, response):
+        ss = json.loads(response.body)
+        bloglist = ss['data']['cards'][0]['card_group']
+
+        for i in bloglist:
+            yield scrapy.Request('https://m.weibo.cn/detail/'+i['mblog']['id'], callback=self.root_knot)
+        for i in range(2, 5):
+            try:
+                yield scrapy.Request(response.url+'&page='+str(i), callback=self.search_url)
+            except:
+                continue
+
+    def search_url(self, response):
+        ss = json.loads(response.body)
+        bloglist = ss['data']['cards'][0]['card_group']
+        for i in bloglist:
+            yield scrapy.Request('https://m.weibo.cn/detail/'+i['mblog']['id'], callback=self.root_knot)
+
+    def root_knot(self, response):
+        retweet = response.text.find("retweeted_status")
+        if retweet == -1:
+            print('原创')
+            render_data = re.findall(
+                'render_data=\[(.+)\]\[0\]\|\|', response.text.replace(' ', '').replace('\n', ''))[0]
+
+            data = json.loads(render_data)
+            status = data['status']
+
+            item = WeiboItem()
+            item['mid'] = status['id']
+            item['userid'] = status['user']['id']
+            item['verified_type'] = status['user']['verified_type']
+            item['text'] = status['text']
+            item['created_at'] = status['created_at']
+            item['created_at'] = time.strftime(
+                '%Y-%m-%d %H:%M:%S', time.strptime(item['created_at'], '%a%b%d%H:%M:%S%z%Y'))
+
+            print('mid:{} userid:{} V{} created_at:{}'.format(
+                item['mid'], item['userid'], item['verified_type'], item['created_at']))
+            print('转发：{} 评论：{} 赞：{}'.format(
+                status['reposts_count'], status['comments_count'], status['attitudes_count']))
+            yield item
+
+        else:
+            temp = response.text[retweet:retweet+200]
+            pid = re.findall(r'"id":.+"(.*?)",', temp, re.S)[0]
+            print('转发', pid)
+            yield scrapy.Request('https://m.weibo.cn/detail/'+pid, callback=self.root_knot)
